a9240c33c87498699ff898fa09bce7c1087b36e8
Mark Lagarta <mj,lagarta00@gmail.com>
2025-02-28 23:12:55 +0800

    Update - Added sticky header to resolve UX issue on navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Scroll } from '../utils/Scroll'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
